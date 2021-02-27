f1e12dfa6c0168483fe5d6ba26a41971dd8016e2
sheepkill15 <simonarons15@gmail.com>
2021-02-27 19:09:03 +0200

    Set up Projects panel

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import ProjectsPanel from './ProjectPanel';
 import CodeRunner from './runcode';
 
 // this method is called when your extension is activated
@@ -22,6 +23,10 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 	});
 	context.subscriptions.push(disposable);
+
+	context.subscriptions.push(vscode.commands.registerCommand('projcpp.projects', () => {
+		ProjectsPanel.createOrShow(context.extensionUri);
+	}));
 }
 
 // this method is called when your extension is deactivated
